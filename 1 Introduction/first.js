15550eac468ad418b7798c675d3fab80fd47880f
Prashant Jain <prashant@knowledgegate.in>
2024-05-26 08:31:24 +0530

    Introduction

---
+++ 1 Introduction/first.js
@@ -0,0 +1,3 @@
+console.log("This will be executed by node");
+console.log(4+5*20);
+console.error('Just want to see what will happen.');
---
+++ 1 Introduction/script.js
@@ -0,0 +1,8 @@
+// This is my first function
+function myFunction() {
+  /*
+  ! asdf
+ a asdf
+  */
+  document.getElementById('first').innerHTML = 'Text changed.';
+}
